2441676d613c5c020cb418635f9dbfae43e618ed
NazarBylen <nazar.bylen.ir.2021@lpnu.ua>
2022-06-09 21:18:02 +0300

    print problems resolved (now it prints good stuff)))

--- main.py
+++ main.py
@@ -12,12 +12,15 @@ def main():
     #zipper()
     txt = open('access.log.txt')
     # print(txt.read())
-    check = re.findall('.*?\[((0[7]\/Mar\/2009:((1[2]:((3[4]:2[6-9])|(3[5-9]:\d{2})))|(1[3-9]|2[0-4]):\d{2}:\d{2}))|'
-                       '(0[8]\/Mar\/2009:((([0-1][0-9]|2[0-2]):\d{2}:\d{2})|((2[3]:[0-3][0-3]:\d{2})|(2[3]:3[4]:(0[1-9]|1[0-1]))))))'
-                       '.*GET.*?([T][W]iki).*?"\s200', txt.read())
+    regex = re.compile('.*?\[((0[7]\/Mar\/2009:((1[2]:((3[4]:2[6-9])|(3[5-9]:\d{2})))|(1[3-9]|2[0-4]):\d{2}:\d{2}))|'
+            '(0[8]\/Mar\/2009:((([0-1][0-9]|2[0-2]):\d{2}:\d{2})|((2[3]:[0-3][0-3]:\d{2})|(2[3]:3[4]:(0[1-9]|1[0-1]))))))'
+            '.*GET.*?([T][W]iki).*?"\s200')
 
-    print(check)
-    print(len(check))
+    with open('access.log.txt', 'r') as file:
+        for line in file:
+            if regex.search(line) is not None:
+                print(line)
+
     txt.close()
 
 
